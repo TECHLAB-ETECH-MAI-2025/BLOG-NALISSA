24b43dfe8c8acd83d39814b8f619f8bd555bc99a
Waindelly Nalissa <nhalyelantomalala@gmail.com>
2025-05-21 01:54:24 +0300

    Modification du template de base et mise à jour du contrôleur et de l'entité Article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,11 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 final class BlogController extends AbstractController
 {
@@ -26,15 +32,52 @@ final class BlogController extends AbstractController
     public function home(){
         return $this->render('blog/home.html.twig');
     }
+      #[Route('/blog/new', name: 'blog_create')]
+      #[Route('/blog/{id}/edit', name: 'edit')]
+    public function create(?Article $article = null,  Request $request, EntityManagerInterface $manager)    {
+       
+    if (!$article) {
+        $article = new Article();
+    }
+
+    //    $form = $this->createFormBuilder($article)
+    //                 ->add(  'title')         
+    //                 ->add('content')         
+    //                 ->add('image')    
+    //                 // ->add('save', SubmitType::class, [
+    //                 //     'label' =>'Save'
+    //                 // ])     
+    //                 ->getForm();
+     $form = $this->createForm(ArticleType::class, $article); 
+                    
+        $form->handleRequest(($request));
+      
+        if($form->isSubmitted() && $form->isValid()){
+            if(!$article->getId()){
+
+                $article->setCreatedAt((new \DateTime()));
+            }
+            $manager->persist(($article));
+            $manager->flush();
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+      
+        return $this ->render('blog/create.html.twig', [
+            'formArticle' => $form->createView(),
+            'editMode' =>$article->getId() !== null
+        ]);
+    }
+
     
     #[Route('/blog/{id}', name: 'blog_show')]
-
+    
     public function show(Article $article){           
 
         return $this->render('blog/show.html.twig', [
-             'article' => $article,
-
+            'article' => $article,
+            
         ]);
-    
+        
     }
+  
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 class Article
@@ -14,16 +15,29 @@ class Article
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Length(min:10,
+     max:255 ,
+     minMessage:"Votre titre est trop courte" )]
     private ?string $title = null;
 
     #[ORM\Column(type: 'text')]
+    #[Assert\Length(
+     min:10,
+     max:255,
+     minMessage:"Votre contenu est trop courte" )]
+
     private ?string $content = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Url()]
     private ?string $image = null;
 
     #[ORM\Column]
     private ?\DateTime $createdAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'articles')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $Catogory = null;
 
     public function getId(): ?int
     {
@@ -74,6 +88,18 @@ class Article
     public function setCreatedAt(\DateTime $createdAt): static
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getCatogory(): ?Category
+    {
+        return $this->Catogory;
+    }
+
+    public function setCatogory(?Category $Catogory): static
+    {
+        $this->Catogory = $Catogory;
 
         return $this;
     }
