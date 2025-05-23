86c960aacc7de138d2a5b7c674755976c4bdf046
Waindelly Nalissa <nhalyelantomalala@gmail.com>
2025-05-24 00:48:27 +0300

    chore(frontend): ajout de Webpack Encore et des nouveaux fichiers d'assets

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// webpack.config.js
+	const Encore = require('@symfony/webpack-encore');
+
+	if (!Encore.isRuntimeEnvironmentConfigured()) {
+		Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'developmentev');
+	}
+
+	Encore
+		.setOutputPath('public/build/')
+		.setPublicPath('/build')
+		.addEntry('app', './assets/app.js')
+		.addEntry('article', './assets/article.js')
+		.addEntry('article_list', './assets/article_list.js')
+		.splitEntryChunks()
+		.enableSingleRuntimeChunk()
+		.cleanupOutputBeforeBuild()
+		.enableBuildNotifications()
+		.enableSourceMaps(!Encore.isProduction())
+		.enableVersioning(Encore.isProduction())
+		.configureBabelPresetEnv((config) => {
+			config.useBuiltIns = 'usage';
+			config.corejs = 3;
+		})
+		.enableSassLoader()
+	;
+module.exports = Encore.getWebpackConfig();
+// const Encore = require('@symfony/webpack-encore');
+
+// Encore
+//   .setOutputPath('public/build/')
+//   .setPublicPath('/build')
+//   .addEntry('app', './assets/app.js')
+//   .enableSingleRuntimeChunk()
+//   .enableSassLoader()  // <-- ajout ici
+// ;
+
+// module.exports = Encore.getWebpackConfig();
