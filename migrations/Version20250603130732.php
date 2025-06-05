29e638e230b1571567043a1a7fd7e4db5eef83df
Waindelly Nalissa <nhalyelantomalala@gmail.com>
2025-06-06 01:14:25 +0300

    Migration et configuration du webpack

---
+++ migrations/Version20250603130732.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250603130732 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE comment ADD CONSTRAINT FK_9474526CF675F31B FOREIGN KEY (author_id) REFERENCES user (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_9474526CF675F31B ON comment (author_id)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE comment DROP FOREIGN KEY FK_9474526CF675F31B
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP INDEX IDX_9474526CF675F31B ON comment
+        SQL);
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,115 +1,37 @@
-// webpack.config.js
-	const Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore');
 
-
-	if (!Encore.isRuntimeEnvironmentConfigured()) {
-		Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'developmentev');
-	}
-
-// Manually configure the runtime environment if not already configured yet by the "encore" command.
-// It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
 Encore
-    // directory where compiled assets will be stored
     .setOutputPath('public/build/')
-    // public path used by the web server to access the output path
     .setPublicPath('/build')
-    // only needed for CDN's or subdirectory deploy
-    //.setManifestKeyPrefix('build/')
 
-    /*
-     * ENTRY CONFIG
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-     */
+    // JS principal
     .addEntry('app', './assets/app.js')
-    .addEntry('app', './assets/styles/app.scss')
+
+    // SCSS en style séparé (évite conflit d'entrée)
+    .addStyleEntry('app_styles', './assets/styles/app.scss')
+
+    // Autres entrées JS
     .addEntry('datatables', './assets/datatables.js')
     .addEntry('articles_list', './assets/articles_list.js')
-    .addEntry('like', './assets/like.js')
-    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    // .addEntry('like', './assets/like.js')
+    .addEntry('article', './assets/article.js')
+    // .addEntry('article_list', './assets/article_list.js')
+
     .splitEntryChunks()
-
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
-
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
     .cleanupOutputBeforeBuild()
-
-    // Displays build status system notifications to the user
-    // .enableBuildNotifications()
-
+    .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
-
-    // configure Babel
-    // .configureBabel((config) => {
-    //     config.plugins.push('@babel/a-babel-plugin');
-    // })
-
-    // enables and configure @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
-        config.corejs = '3.38';
+        config.corejs = 3;
     })
-
-    // enables Sass/SCSS support
     .enableSassLoader()
-
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
-
-    // uncomment if you use React
-    //.enableReactPreset()
-
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
-
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
 ;
 
-
-	Encore
-		.setOutputPath('public/build/')
-		.setPublicPath('/build')
-		.addEntry('app', './assets/app.js')
-		.addEntry('article', './assets/article.js')
-		.addEntry('article_list', './assets/article_list.js')
-		.splitEntryChunks()
-		.enableSingleRuntimeChunk()
-		.cleanupOutputBeforeBuild()
-		.enableBuildNotifications()
-		.enableSourceMaps(!Encore.isProduction())
-		.enableVersioning(Encore.isProduction())
-		.configureBabelPresetEnv((config) => {
-			config.useBuiltIns = 'usage';
-			config.corejs = 3;
-		})
-		.enableSassLoader()
-	;
 module.exports = Encore.getWebpackConfig();
-// const Encore = require('@symfony/webpack-encore');
-
-// Encore
-//   .setOutputPath('public/build/')
-//   .setPublicPath('/build')
-//   .addEntry('app', './assets/app.js')
-//   .enableSingleRuntimeChunk()
-//   .enableSassLoader()  // <-- ajout ici
-// ;
-
-// module.exports = Encore.getWebpackConfig();
