f5fbcf8ad679eb4b607bce3657319a5bce5a0602
Waindelly Nalissa <nhalyelantomalala@gmail.com>
2025-06-11 00:51:40 +0300

    fusionner feature/general-cleanup dans master

--- assets/datatables.js
+++ assets/datatables.js
@@ -35,23 +35,5 @@ $(function () {
             },
             responsive: true
         });
-        }
-
-
-
-    console.log("Initialisation de DataTables...");
-    $('#articlesTable').DataTable({
-        ajax: "{{ path('api_articles') }}",
-        columns: [
-            { data: 'id' },
-            { data: 'title' },
-            { data: 'content' },
-            { data: 'createdAt' }
-            // { data: 'actions' }
-        ],
-        language: {
-            url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/fr-FR.json'
-        },
-        responsive: true
-    });
+    }
 });
