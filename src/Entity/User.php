77043b166ac9b4d5c5bfa5a6d66c7aebc0b7a016
Waindelly Nalissa <nhalyelantomalala@gmail.com>
2025-06-11 03:44:15 +0300

    feat: ajoute firstName et lastName dans User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -178,4 +178,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+    public function getFullName(): string
+{
+    return trim($this->firstName . ' ' . $this->lastName);
+}
+
 }
